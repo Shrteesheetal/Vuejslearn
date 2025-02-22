f5fa4ea020f844c5277ae7464e81cbbdf8a8f2ff
Shrteesheetal <info.shreesheetalshree@gmail.com>
2025-02-22 21:49:35 +0545

    Added new vs project

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Code Step By Step.js App"/> -->
+  <HomeScreen />
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import HomeScreen from './components/home.vue'
+
+export default {
+  name: 'App',
+  components: {
+   // HelloWorld,
+    HomeScreen,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/home.vue
@@ -0,0 +1,60 @@
+<template>
+    <h1 v-on:mousemove="getConsole()">Home Components{{ 10+10 }}</h1> 
+
+    <h1>Email:{{ email }}</h1>
+    <h1>Mobile:{{ mobile}}</h1>
+    <h1>Name:{{getName("Sidhu")}}</h1>
+    <h1>Name :{{ getName("peter") }}</h1>
+    <h1>All Data:{{ getData() }}</h1>
+    <button v-on:click="getData('Button 1 Clicked')">Click Me</button>
+    <button v-on:dblclick="getData('Button double Clicked')">Click Me</button>
+    <h2>{{ count }}</h2>
+
+</template>
+
+
+<script>
+export default {
+    name:'HomeScreen',
+    data()
+    {
+        return{
+            email:'nil@test.com',
+            mobile:9898,
+            getName:function(a)
+            {
+                return a;
+            }
+
+        }
+    }
+    ,
+    methods:{
+      
+        getData()
+        {
+            return{
+                name:'anil',
+                email:'anil@sidhu.com'
+            }
+        },
+        getConsole()
+        {
+            console.warn("function called")
+        }
+
+    }
+
+
+}
+</script>
+<style scoped>
+h1{
+    color:orange
+    
+
+
+    
+
+}
+</style>
